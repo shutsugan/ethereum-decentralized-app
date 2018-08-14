9812a4fabcbb6a7f5e6bc2bda33cc25afea922d1
Muhammad Tajjiou <shutsugan@gmail.com>
2018-08-14 17:14:05 +0100

    Create a Candidate struct in the Election contract, map it to candidates, init a candidatesCount to count the added candidates, create an addCandidate function that increments the candidatesCount and add the candidate based on it, and add two candidates in the constructor before deployment to the blockchain

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,31 @@
 pragma solidity ^0.4.0;
 
 contract Election {
+    //Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
     //Read/write candidate
-    string public candidate;
+    mapping(uint => Candidate) public candidates;
+
+    //Store CandidatesCount;
+    uint public candidatesCount;
 
     //Constructor
     constructor() {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(
+            candidatesCount,
+            _name,
+            0
+        );
     }
 }

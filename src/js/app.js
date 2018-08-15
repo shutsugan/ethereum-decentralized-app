78090bc83c2f272878780957013bf20f2847c98c
Muhammad Tajjiou <shutsugan@gmail.com>
2018-08-15 11:07:46 +0100

    Build the client-side app that will talk to the smart contract, restructure the html provided by truffle, add style, set up the web3, initialize the contracts and render the data from the smart contract

--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,93 @@
 App = {
   web3Provider: null,
   contracts: {},
+  account: '0x0',
 
   init: function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
     return App.initWeb3();
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider)
+    }
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    const url = 'Election.json';
+    const cb = election => {
+      console.log(election);
+      App.contracts.Election = TruffleContract(election);
+      App.contracts.Election.setProvider(App.web3Provider);
 
-    return App.bindEvents();
+      return App.render();
+    };
+    getJson(url, cb);
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
+  render: function() {
+    let electionInstance;
+    const loader = document.querySelector('.loader');
+    const container = document.querySelector('.rows__main');
+    const content = document.querySelector('.account');
+    loader.classList.add('show');
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
+    //Load account data
+    web3.eth.getCoinbase((err, account) => {
+      if (err !== null) return;
 
-  handleAdopt: function(event) {
-    event.preventDefault();
+      App.account = account;
+      content.innerHTML = `Your Account: ${account}`;
+    });
 
-    var petId = parseInt($(event.target).data('id'));
+    //Load contract data
+    App.contracts.Election
+      .deployed()
+      .then(instance => {
+        electionInstance = instance;
+        return electionInstance.candidatesCount();
+      })
+      .then(candidatesCount => {
+        container.innerHTML = '';
 
-    /*
-     * Replace me...
-     */
+        for (let i = 1; i <= candidatesCount; i++) {
+          electionInstance
+            .candidates(i)
+            .then(candidate => {
+              const id = candidate[0];
+              const name = candidate[1];
+              const voteCount = candidate[2];
+
+              const template = `
+                <div class="rows__item-container sides-padding">
+                  <div class="rows__item">${id}</div>
+                  <div class="rows__item">${name}</div>
+                  <div class="rows__item">${voteCount}</div>
+                </div>
+              `;
+
+              container.insertAdjacentHTML('beforeend', template);
+            })
+        }
+
+        loader.classList.remove('show');
+      })
+      .catch(err => console.warn(err));
   }
-
 };
 
-$(function() {
-  $(window).load(function() {
-    App.init();
-  });
+window.addEventListener('load', _ => {
+  App.init();
 });
+
+function getJson(url, cb) {
+  fetch(url)
+    .then(res => res.json())
+    .then(data => cb(data));
+}
